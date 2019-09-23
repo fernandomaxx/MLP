2aaecdbbfabf27ec35fc4b8e2f9ccc7efd49bb9f
joseildo filho <joseildofilho@yahoo.com.br>
2019-09-23 20:32:29 -0300

    * cache.py     - This file has 2 classes 	- The class Cache is responsable to store and calculate the 	diferrence between solutions 	- SubSolution it's a helper to the Cache, because store the 	tuple of each subsolution and provides a lot of funcionalities

---
+++ cache.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+T = 0
+C = 1
+W = 2
+
+class Cache(object):
+
+    def __init__(self, adj_matrix):
+        '''
+            Object that store and manage an solution helper.
+            n: Size of the problem
+        '''
+        self._adj_matrix = adj_matrix
+        self._n = len(adj_matrix) + 1
+        self._table = []
+
+    def update(self, solution):
+        '''
+            updates the state to a given solution
+            solution: a list with a path
+        '''
+        self._table = []
+
+        for i in range(self._n):
+            self._table.append(
+                    [SubSolution(i, 1, self._adj_matrix, solution)]
+                    )
+
+        for i in range(self._n - 1):
+            for j in range(i+1, self._n):
+                self._table[i].append(self._table[i][-1] + self._table[j][0])
+
+    def compare(self, sigmas):
+        '''
+            Calculates the new Solution in the cost of the parts that were moved
+            sigmas: a list of tuples, where each tuple contains a position in solution and the size of the solution
+        '''
+        i,j = sigmas.pop(0)
+        new_sol = self._table[i][j-1]
+        for i,j in sigmas:
+            new_sol += self._table[i][j-1]
+        return new_sol
+
+class SubSolution(object):
+    def __init__(self, start, size, adj_matrix, solution, T=0, C=0, W=1):
+        self.start = start
+        self.size = size
+        self.T = T
+        self.C = C
+        self.W = W if start else 0
+        self._adj_matrix = adj_matrix
+        self._solution = solution
+
+    def __add__(self, other):
+        sub_solution = SubSolution(self.start, 
+                self.size + other.size,
+                self._adj_matrix, 
+                self._solution)
+        i = self._solution[self.start + self.size - 1]
+        j = self._solution[self.start + self.size]
+        t = self._adj_matrix[i, j]
+        sub_solution.T = self.T + t + other.T
+        sub_solution.C = self.C + other.W * (self.T + t) + other.C
+        sub_solution.W = self.W + other.W
+
+        return sub_solution
+
+    def __str__(self):
+        return "T={} C={} W={}".format(self.T, self.C, self.W)
+
