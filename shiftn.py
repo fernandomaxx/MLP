73694da506f3147ea3f53d419a4217bb8c4aa161
joseildo filho <joseildofilho@yahoo.com.br>
2019-09-27 11:56:57 -0300

    generalization to be shift n

--- shiftn.py
+++ shiftn.py
@@ -2,15 +2,16 @@ from neighborhoods import Neighborhood
 
 class Shift(Neighborhood):
     def __init__(self, cache, n=1):
+        assert n > 0
         self._cache = cache
         self.n = n
 
-    def execute(self, solution, block_position):
+    def execute(self, solution):
         best = self._cache.compare([(0, len(solution))])
         best_sol = solution
-        for i in range(1, len(solution)-1):
-            right_lim = len(solution) - i - 1
-            left_lim  = len(solution) - right_lim - 1 
+        for i in range(1, len(solution)-self.n):
+            right_lim = len(solution) - i - self.n
+            left_lim  = len(solution) - right_lim - self.n 
 
             left_size  = 1
             mid_right_size = left_lim - 1
@@ -21,12 +22,13 @@ class Shift(Neighborhood):
             right_pos = len(solution) - right_size
             left_pos = 1
 
-            for j in range(len(solution) - 2):
+            for j in range(len(solution) - 1 - self.n):
                 sol = [(0, left_size),
-                        (i+1, mid_left_size),
-                        (i,1), # position of the shifted
+                        (i+self.n, mid_left_size),
+                        (i,self.n), # position of the shifted
                         (left_pos, mid_right_size),
                         (right_pos,right_size)]
+                print(sol)
 
                 if right_size - 1 >= 0 and left_size == left_lim:
                     right_size -= 1; mid_left_size += 1
@@ -40,10 +42,10 @@ class Shift(Neighborhood):
                 if best.C > aux.C:
                     best = aux
                     best_sol = sol
+            print()
         sol = []
         for i,j in best_sol:
             if j:
                 for k in solution[i:i+j]:
                     sol.append(k)
-        return sol
-
+        return best_sol
