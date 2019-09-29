74a77c9f549fd9347551a30b5690e03296976ce1
fernandomaxx <fernando_maxx@cc.ci.ufpb.br>
2019-09-29 15:36:24 -0300

    metaheuristic added

---
+++ ils.py
@@ -0,0 +1,54 @@
+from greedy_construction import GreddyConstruction
+from rvnd import RVND
+from tools import f
+
+class ILS(object):
+
+    def __init__(self, adj_list, adj_matrix, Max, MaxIls):
+        self.Max = Max
+        self.MaxIls = MaxIls
+        self._adj_list = adj_list
+        self._adj_matrix = adj_matrix
+        self.gd = GreddyConstruction(adj_list,
+            len(adj_matrix))
+
+    def procedure(self):
+        best_ = 100000
+        best_sol = []
+
+        for _ in range(self.Max):
+            new_sol = []
+            self.gd.build(1, new_sol)
+            #Entrada que o shift 2 piora
+            #new_sol = [0, 2, 4, 1, 3, 0]
+            new_ = f(new_sol, self._adj_matrix)
+            aux_sol = new_sol
+            aux_ = new_
+
+            #debug
+            print('\n{}----------------'.format(_))
+            print('{}: {}'.format(new_sol, new_))
+            
+            for iterIls in range(self.MaxIls):
+                #debug
+                print((new_sol, new_))
+
+                new_sol = RVND(new_sol, self._adj_matrix).execute()
+                new_ = f(new_sol, self._adj_matrix)
+                #debug
+                print((new_sol, new_))
+                
+                if aux_ > new_:
+                    aux_ = new_
+                    aux_sol = new_sol
+                    iterIls = 0
+
+                #falta a pertubação
+
+            if best_ > aux_:
+                best_sol = aux_sol
+                best_ = aux_
+
+        return (best_sol, best_)
+
+                
